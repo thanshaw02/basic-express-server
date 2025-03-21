cf62e8adcee05bc43b11cb836b6afdc3faa26ca6
Tylor Hanshaw <tylorjhanshaw@xeratec.com>
2025-03-21 16:34:02 -0400

    initial commit for basic express server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const PORT = 8000;
+
+router.get("/ping", (req, res) => {
+    console.log("Ping health check made on server");
+    res.send({ status: 200, message: "Healthy" });
+});
+
+app.use("/api", router);
+
+app.listen(PORT, () => {
+    console.log("===========================================================");
+    console.log(`============ Server is listening on port: ${PORT} ============`);
+    console.log("===========================================================");
+});
